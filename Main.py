3b2e244fe1088d979a62391d433e4ac7ddbea93b
camijavi <camijavinavarro@gmail.com>
2026-08-25 07:02:52 -0600

    initial commit, set functions for simple if exercises

---
+++ Main.py
@@ -0,0 +1,8 @@
+from SimpleIf import stockManagement
+
+def main():
+
+    # Use match case 
+    cornerstoreStockManagement()
+
+main()
---
+++ SimpleIf.py
@@ -0,0 +1,14 @@
+def cornerstoreStockManagement():
+    stock = 10
+
+def storePromotion():
+    discount = 100
+
+def salesGoal():
+    lala
+
+def diningRoomSetDelivry():
+    lala = "123"
+
+def productWeight():
+    lala = "lala"
