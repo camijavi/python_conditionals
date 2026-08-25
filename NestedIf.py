90cd7ef31563c1b79a6f457713a01d653a2ed67a
camijavi <camijavinavarro@gmail.com>
2026-08-25 07:09:07 -0600

    add functions for storeCredit, deliveryService, coffeeGrading, accommodationBooking, and hardwareSales

---
+++ NestedIf.py
@@ -0,0 +1,14 @@
+def storeCredit():
+    lalal = "example"
+
+def deliveryService():
+    wall = "hello"
+
+def coffeeGrading():
+    coffee="camila"
+
+def accommodationBooking():
+    hotel="hotel"
+
+def hardwareSales():
+    hardware=2
