8949e21f5d0f0d5179de2bd5b5e5098da6b61330
camijavi <camijavinavarro@gmail.com>
2026-08-31 08:35:10 -0600

    implement boilerplate structures for for-loop and while-loop exercise modules

---
+++ ForExercises.py
@@ -0,0 +1,16 @@
+from Components import clearConsole 
+
+def miniSuperSales():
+    clearConsole()
+
+def coffeeReception():
+    clearConsole()
+
+def stockChecking():
+    clearConsole()
+
+def breadProduction():
+    clearConsole()
+
+def serviceEvaluation():
+    clearConsole()
---
+++ WhileExercises.py
@@ -0,0 +1,16 @@
+from Components import clearConsole 
+
+def cashClosing():
+    clearConsole()
+
+def systemAccess():
+    clearConsole()
+
+def orderQty():
+    clearConsole()
+
+def deliveryGas():
+    clearConsole()
+
+def stockReplenishment():
+    clearConsole()
