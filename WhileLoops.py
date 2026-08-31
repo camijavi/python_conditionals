00c2990a3a1a0bc81835ef172ad2418ca91e8141
camijavi <camijavinavarro@gmail.com>
2026-08-31 09:56:18 -0600

    implement correct login logic with attempt tracking in systemAccess

--- WhileLoops.py
+++ WhileLoops.py
@@ -4,9 +4,6 @@ def cashClosing():
     clearConsole()
 
 def systemAccess():
-# Tu misión: Solicita la clave hasta que sea correcta. 
-# Cuenta los intentos e informa cuántos fueron necesarios.
-
     clearConsole()
     print("INGRESE SUS CREDENCIALES")
     username = input("Usuario: ")
@@ -15,18 +12,18 @@ def systemAccess():
     attempts = 0 
 
     while loggedIn != True:
-        if  username == camijavi:
+        if  username == "camijavi":
             password = input("Contraseña: ")
-            if password == 123456:
-                continue
+            if password == "123456":
+                loggedIn = True
+                print(f"BIENVENIDO DE VUELTA! {username}")
             else:
                 print("CONTRASEÑA INCORRECTA")
                 attempts+= 1
         else:
             print("USUARIO INCORRECTA")
             attempts+= 1
-            loggedIn = True
-            print(f"BIENVENIDO DE VUELTA! {username}")
+    return(f"Se necesitaron {attempts} intentos para iniciar sesión")
 
 
 def orderQty():
